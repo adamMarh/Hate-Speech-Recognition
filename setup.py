7378b7ca44819b804303b4ecfccd278030a9e2a5
Adam <adam.mrh@outlook.com>
2025-11-09 00:59:40 -0500

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Adam Marhraoui",
+    author_email="adam.mrh@outlook.com",
+    packages=find_packages(),
+    install_requires=[],
+)
